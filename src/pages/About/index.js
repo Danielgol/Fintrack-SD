bec29807ce2954596017ffd8d5f771cfb5bda09e
Diego Pires <diegospf13@gmail.com>
2023-02-05 21:45:12 -0300

    Conteúdo da tela Sobre/Informações

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,56 @@
 import * as React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function About() {
   return (
+    <ScrollView>
     <View style={styles.about}>
-        <Text style={{color: 'white' }}>Sobre o proeto e sobre nós.</Text>
+      <LinearGradient colors={['#69279c', '#6a2fa3', '#6a37aa', '#6a3eb1', '#6a46b8', '#6a4cbf', '#6a53c6', '#6a5acd']} style={styles.card}>
+       <Text style={styles.textTitle}>Propósito</Text>
+       <Text style={styles.text}>O aplicativo Fintrack tem como objetivo ajudar as pessoas a aprenderem sobre educação financeira de maneira simples e intuitiva. Com ele, é possível visualizar as cotações de ativos financeiros e descobrir onde é o melhor lugar para investir seu dinheiro. Além disso, o Fintrack oferece recursos e ferramentas que ajudam na compreensão dos conceitos financeiros, tornando o processo de aprendizado mais acessível. O propósito do aplicativo é democratizar o conhecimento financeiro e ajudar as pessoas a alcançarem suas metas financeiras de maneira mais eficiente.</Text>
+      </LinearGradient>
+      <View>
+        <LinearGradient colors={['#69279c', '#6a2fa3', '#6a37aa', '#6a3eb1', '#6a46b8', '#6a4cbf', '#6a53c6', '#6a5acd']} style={styles.card}>
+          <Text style={styles.textTitle}>Quem somos?</Text>
+          <Text style={styles.text}>Somos uma equipe de estudantes da Universidade Federal Rural de Pernambuco (UFRPE), matriculados no curso de Ciência da Computação desde o semestre 2019.1. Temos como objetivo aprimorar nossos conhecimentos em tecnologia e desenvolvimento de software, além de buscar soluções inovadoras para problemas do dia a dia. Nossa formação acadêmica nos permite entender e aplicar as últimas tendências em tecnologia de maneira eficiente e efetiva. Acreditamos na importância da tecnologia para melhorar a qualidade de vida das pessoas e buscamos colaborar para isso com o Fintrack.</Text>
+        </LinearGradient>
+        <View style={styles.profileContainer}>
+            <LinearGradient colors={['#69279c', '#6a2fa3', '#6a37aa', '#6a3eb1', '#6a46b8', '#6a4cbf', '#6a53c6', '#6a5acd']} style={styles.profile}>
+              <Image style={styles.imageProfile} source={require('../../../assets/images/daniel.png')}></Image>
+              <View style={styles.contact}>
+                <Text style={styles.name}>Daniel Rosa</Text>
+                <View style={styles.buttons}>
+                  <Icon style={styles.button} name="logo-instagram" size={25} color="black"/>
+                  <Icon style={styles.button} name="logo-linkedin" size={25} color="black"/>
+                </View>
+              </View>
+            </LinearGradient>
+            <LinearGradient colors={['#69279c', '#6a2fa3', '#6a37aa', '#6a3eb1', '#6a46b8', '#6a4cbf', '#6a53c6', '#6a5acd']} style={styles.profile}>
+              <Image style={styles.imageProfile} source={require('../../../assets/images/diego.jpg')}></Image>
+              <View style={styles.contact}>
+                <Text style={styles.name}>Diego Pires</Text>
+                <View style={styles.buttons}>
+                  <Icon style={styles.button} name="logo-instagram" size={25} color="black"/>
+                  <Icon style={styles.button} name="logo-linkedin" size={25} color="black"/>
+                </View>
+              </View>
+            </LinearGradient>
+            <LinearGradient colors={['#69279c', '#6a2fa3', '#6a37aa', '#6a3eb1', '#6a46b8', '#6a4cbf', '#6a53c6', '#6a5acd']} style={styles.profile}>
+             <Image style={styles.imageProfile} source={require('../../../assets/images/joao.jpg')}></Image>
+             <View style={styles.contact}>
+              <Text style={styles.name}>João Cavalcanti</Text>
+              <View style={styles.buttons}>
+                <Icon style={styles.button} name="logo-instagram" size={25} color="black"/>
+                <Icon style={styles.button} name="logo-linkedin" size={25} color="black"/>
+              </View>
+             </View>
+            </LinearGradient>
+        </View>
+      </View>
     </View>
+    </ScrollView>
   );
 }
 
@@ -14,4 +59,60 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(34,36,40,1)',
     height: '100%'
   },
+  card:{
+    backgroundColor: 'purple',
+    margin:20,
+    marginHorizontal: 40,
+    borderRadius: 15,
+    padding: 20,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  textTitle:{
+    color:'white',
+    fontSize: 25,
+    fontFamily: 'Jost-Bold'
+  },
+  text:{
+    marginTop:10,
+    color: 'white',
+    textAlign: 'left',
+    fontFamily: 'Jost-Medium'
+  },
+  profile:{
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 20,
+    marginHorizontal:40,
+    padding:20,
+    borderRadius:15,
+    backgroundColor: 'purple',  
+  },
+  imageProfile:{
+    width:130,
+    height:130,
+    borderRadius:100
+  },
+  contact:{
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  name:{
+    fontFamily:'Jost-SemiBold',
+    color: 'white',
+  },
+  buttons:{
+    margin:20,
+    width:90,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button:{
+    padding:5,
+    backgroundColor: '#D6BBE6',
+    borderRadius:5
+  }  
 });
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Home() {
 const navigation = useNavigation();
+
   return (
     <ScrollView style={styles.home}>
      <View style={styles.card}>
@@ -13,43 +14,43 @@ const navigation = useNavigation();
           <LinearGradient colors={['#00aaff', '#15a0fb', '#2895f6', '#388af0', '#467fe9', '#5373e1', '#5f67d8','#6a5acd']} style={{borderRadius:15, marginVertical:20}}> 
             <TouchableOpacity style={[styles.cardColumn]} onPress={() => navigation.navigate('Calculadora')}>
                 <Text style={styles.text}>Compare a melhor forma de investir!</Text>  
-                <Icon style={styles.about} name="wallet-sharp" size={40} color="black"/>
+                <Icon name="wallet-sharp" size={40} color="black"/>
             </TouchableOpacity>
           </LinearGradient>
           <LinearGradient colors={['#990094', '#951c9e', '#8f2ba7', '#8937b0', '#8341b8', '#7b4abf', '#7352c7', '#6a5acd']} style={{borderRadius:15, marginVertical:20}}> 
             <TouchableOpacity style={[styles.cardColumn]} onPress={() => navigation.navigate('Perfil')}>
                 <Text style={styles.text}>Qual é seu perfil de investidor?</Text>  
-                <Icon style={styles.about} name="people-sharp" size={40} color="black"/>
+                <Icon name="people-sharp" size={40} color="black"/>
             </TouchableOpacity> 
           </LinearGradient> 
           <LinearGradient colors={['#1fffbc', '#2ce6a6', '#32cd91', '#33b57d', '#319e6a', '#2e8758','#297148', '#235c38']} style={{borderRadius:15, marginVertical:20}}> 
             <TouchableOpacity style={[styles.cardColumn]} onPress={() => navigation.navigate('Estudo')}>
                 <Text style={styles.text}>Aprenda agora!</Text>
-                <Icon style={styles.about} name="book-sharp" size={40} color="black"/>  
+                <Icon name="book-sharp" size={40} color="black"/>  
             </TouchableOpacity>
           </LinearGradient> 
           <LinearGradient colors={['#ffb56b', '#f39060', '#e16b5c', '#ca485c', '#ac255e', '#870160']} style={{borderRadius:15, marginVertical:20}}>   
             <TouchableOpacity style={[styles.cardColumn]} onPress={() => navigation.navigate('Cotações')}>
                 <Text style={styles.text}>Cotações do momento!</Text>  
-                <Icon style={styles.about} name="trending-up-sharp" size={40} color="black"/>
+                <Icon name="trending-up-sharp" size={40} color="black"/>
             </TouchableOpacity>  
           </LinearGradient> 
           <LinearGradient colors={['#d4d4d4', '#c7c2d4', '#bab0d3', '#ac9ed2', '#9d8dd1', '#8d7cd0', '#7d6bcf', '#6a5acd']} style={{borderRadius:15, marginVertical:20}}> 
             <TouchableOpacity style={[styles.cardColumn]} onPress={() => navigation.navigate('Calculadora')}>
                 <Text style={styles.text}>Calcule qualquer tipo de juros!</Text>  
-                <Icon style={styles.about} name="calculator-sharp" size={40} color="black"/>
+                <Icon name="calculator-sharp" size={40} color="black"/>
             </TouchableOpacity>  
           </LinearGradient> 
           <LinearGradient colors={['#f2e200', '#f0d821', '#eece32', '#eac43e', '#e4bb48', '#deb251', '#d6aa59', '#cda260']} style={{borderRadius:15, marginVertical:20}}> 
             <TouchableOpacity style={[styles.cardColumn]} onPress={() => navigation.navigate('Cotações')}>
                 <Text style={styles.text}>Valores das Criptomoedas!</Text>  
-                <Icon style={styles.about} name="logo-bitcoin" size={40} color="black"/>
+                <Icon name="logo-bitcoin" size={40} color="black"/>
             </TouchableOpacity> 
           </LinearGradient>  
           <LinearGradient colors={['#f2cede', '#ecc2d9', '#e6b7d5', '#deacd2', '#d5a1d0', '#ca97ce', '#be8ecd', '#b085cd']} style={{borderRadius:15, marginVertical:20}}> 
             <TouchableOpacity style={[styles.cardColumn]} onPress={() => navigation.navigate('Estudo')}>
                 <Text style={styles.text}>Ajuda</Text>  
-                <Icon style={styles.about} name="help-circle" size={40} color="black"/>
+                <Icon name="help-circle" size={40} color="black"/>
             </TouchableOpacity>  
           </LinearGradient> 
         </View>
