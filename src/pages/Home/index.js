6d52e5a61b34b97d2a95d91c86cede804c3c959b
joao.assis <joao.assis@jfpe.jus.br>
2023-03-04 14:18:37 -0300

    Mudança de nome '-'

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,9 +15,9 @@ const navigation = useNavigation();
           <LargeButton text="Compare a melhor forma de investir!" bgColor="#19eb7a" navigate="Calculadora" icon="wallet-sharp"></LargeButton>
           <LargeButton text="Qual é seu perfil de investidor?" bgColor="#d4d4d4" navigate="Perfil" icon="people-sharp"></LargeButton>
           <LargeButton text="Aprenda agora!" bgColor="#dead5b" navigate="Estudo" icon="book-sharp"></LargeButton>
-          <LargeButton text="Cotações do momento!" bgColor="#fff623" navigate="Cotações" icon="trending-up-sharp"></LargeButton>
+          <LargeButton text="Valores das ações!" bgColor="#fff623" navigate="Cotações" icon="trending-up-sharp"></LargeButton>
           <LargeButton text="Calcule qualquer tipo de juros!" bgColor="#45e1f8" navigate="Calculadora" icon="calculator-sharp"></LargeButton>
-          <LargeButton text="Valores das Criptomoedas!" bgColor="#ff8f59" navigate="Cotações" icon="logo-bitcoin"></LargeButton>
+          <LargeButton text="Cotações do momento!" bgColor="#ff8f59" navigate="Cotações" icon="logo-bitcoin"></LargeButton>
           <LargeButton text="Ajuda" bgColor="#f2cede" navigate="Estudo" icon="help-circle"></LargeButton>
         </View>
      </View>
